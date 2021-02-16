bdbb7de00282ce96e3c9b629a35e64b618cac006
Lanove <figoarzaki123@gmail.com>
2021-02-16 22:10:34 +0700

    Added face descriptor batch generator

---
+++ face_modules/modules/canvasPatch.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.canvas = void 0;
+var canvas = require("canvas");
+exports.canvas = canvas;
+var faceapi = require("face-api.js");
+var Canvas = canvas.Canvas, Image = canvas.Image, ImageData = canvas.ImageData;
+faceapi.env.monkeyPatch({ Canvas: Canvas, Image: Image, ImageData: ImageData });
--- face_modules/modules/saveFile.ts
+++ face_modules/modules/saveFile.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const baseDir = path.resolve(__dirname, '../../');
+const baseDir = path.resolve(__dirname, "../../");
 export function saveFile(fileName: string, buf: Buffer) {
   if (!fs.existsSync(baseDir)) {
     fs.mkdirSync(baseDir);
@@ -13,25 +13,33 @@ export function readFile(fileName: string) {
   return fs.readFileSync(path.resolve(baseDir, fileName));
 }
 
-export function readMultipleFiles(filename : string,onFileContent : string,onError : string){
-    console.log(filename,onFileContent,onError);
+export function readMultipleFiles(
+  dirNameRelative: string,
+  onFileContent: (filename: string, content: Buffer) => void,
+  onError: (err: NodeJS.ErrnoException | null) => void
+) {
+  const dirname = path.resolve(baseDir, dirNameRelative);
+  fs.readdir(dirname, function (err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function (filename) {
+      fs.readFile(path.resolve(dirname, filename), function (err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
 }
 
-// function readFiles(dirName : string, onFileContent : (filename: string, content: Buffer) => void, onError : (err: NodeJS.ErrnoException | null) => void) {
-//   const dirname = path.resolve(baseDir, dirName);
-//     fs.readdir(dirname, function (err, filenames) {
-//     if (err) {
-//       onError(err);
-//       return;
-//     }
-//     filenames.forEach(function (filename) {
-//       fs.readFile(path.resolve(dirname, filename), function (err, content) {
-//         if (err) {
-//           onError(err);
-//           return;
-//         }
-//         onFileContent(filename, content);
-//       });
-//     });
-//   });
-// }
+export function listFile(
+  dirNameRelative: string,
+  onError: (err: NodeJS.ErrnoException | null) => void
+): string[] {
+  const dirname = path.resolve(baseDir, dirNameRelative);
+  return fs.readdirSync(dirname);
+}
