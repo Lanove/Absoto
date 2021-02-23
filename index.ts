f27d5cca905def0a89f72ec1b242c0696abfae99
Lanove <figoarzaki123@gmail.com>
2021-02-23 20:00:02 +0700

    face recognition API working

--- index.ts
+++ index.ts
@@ -12,45 +12,39 @@ import * as faceapi from "face-api.js";
 import * as faceDescriptor from "./face_modules/modules/faceDescriptors";
 import * as path from "path";
 import * as os from "os";
+import * as fs from "fs";
 
-// faceDescriptor.generateDescriptor();
-let faceMatcher = faceDescriptor.loadFaceMatcher();
 const bodyParser = require("body-parser");
-let multer = require("multer");
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '.jpeg')
-  }
-})
-let upload = multer({ dest: "uploads/" });
+let multer = require("multer"),
+  upload = multer({ dest: "temp/" }),
+  faceMatcher = faceDescriptor.loadFaceMatcher();
 
 const networkInterfaces = os.networkInterfaces(),
   express = require("express"),
   app = express(),
-  // app2 = express(),
   ip = networkInterfaces["Wi-Fi"][1]["address"].toString(),
   localhost = "localhost",
   port = 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.post("/recognizeFace", upload.single("imageFile"), (req, res) => {
-  res.set("Connection", "application/json");
-  res.set("Content-Type", "keep-alive");
+  res.set("Connection", "keep-alive");
+  res.set("Content-Type", "application/json");
   console.log(req.headers);
   console.log(req.file);
   if (req.file) {
-    recognizeFace(req.file.path,response => {
-      res.send(JSON.stringify(response))
+    recognizeFace(req.file.path, (response) => {
+      if (response.length) res.send(JSON.stringify({status:"SUCCESS",message:response}));
+      else res.send(JSON.stringify({status:"FAIL",message:"No face detected!"}));
+      fs.unlinkSync(req.file.path);
     });
-  } else res.send("ERROR : No image sent");
+  } else res.send(JSON.stringify({status:"FAIL",message:"No image sent!"}));
 });
 
 app.use(express.static(__dirname));
@@ -63,8 +57,11 @@ app.listen(port, ip, () => {
   console.log(`Example app listening at http://${ip}:${port}`);
 });
 
-async function recognizeFace(imagePath: string, callback:(response:faceapi.FaceMatch[]) => void) {
-  let result:faceapi.FaceMatch[] = [];
+async function recognizeFace(
+  imagePath: string,
+  callback: (response: faceapi.FaceMatch[]) => void
+) {
+  let result: faceapi.FaceMatch[] = [];
   await faceNet.loadFromDisk("./face_modules/models");
   await faceLandmark68Net.loadFromDisk("./face_modules/models");
   await faceRecognitionNet.loadFromDisk("./face_modules/models");
@@ -77,15 +74,9 @@ async function recognizeFace(imagePath: string, callback:(response:faceapi.FaceM
     .withFaceLandmarks()
     .withFaceDescriptors();
   if (results) {
-    const queryDrawBoxes = results.map((res) => {
-      const bestMatch = faceMatcher.findBestMatch(res.descriptor);
-      result.push(bestMatch);
-      console.log(bestMatch.toString());
-      // return new faceapi.draw.DrawBox(res.detection.box, { label: bestMatch.toString() })
+    results.map((res) => {
+      result.push(faceMatcher.findBestMatch(res.descriptor));
     });
-  } else console.log("face not found!");
+  } else console.log("No face detected!");
   callback(result);
-  // const outQuery = faceapi.createCanvasFromMedia(img)
-  // queryDrawBoxes.forEach(drawBox => drawBox.draw(outQuery))
-  // fileManager.saveFile('./face_modules/queryImage.jpg', (outQuery as any).toBuffer('image/jpeg'))
 }
